b8a9a1747eaee416e6e46297c78835472aec0534
Micho Radovnikovich <mradov@gmail.com>
2018-05-06 13:44:37 -0400

    Changes to fix build issues; added launch check to sequoia_nav_laser

--- ekf_example/src/EkfExample.h
+++ ekf_example/src/EkfExample.h
@@ -7,7 +7,7 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <sensor_msgs/NavSatFix.h>
-#include <ugv_course_libs/gps_conv.h>
+#include "gps_conv.h"
 #include <dynamic_reconfigure/server.h>
 #include <ekf_example/EkfExampleConfig.h>
 
@@ -38,16 +38,16 @@ private:
   UTMCoords ref_coords;
   tf::TransformBroadcaster broadcaster;
   double sample_time;
-  
+
   Eigen::Matrix<double, 5, 5> Q;
-  
+
   Eigen::Matrix<double, 4, 4> R_all;
   Eigen::Matrix<double, 2, 2> R_gps;
   Eigen::Matrix<double, 2, 2> R_twist;
-  
+
   Eigen::Matrix<double, 5, 1> X;
   Eigen::Matrix<double, 5, 5> P;
-  
+
   bool gps_available;
   bool twist_available;
 
