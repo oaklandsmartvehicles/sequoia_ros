6e1f9d6a6674c7315640838f97fcbbdfe14ccdee
Micho Radovnikovich <mradov@gmail.com>
2018-05-30 21:16:43 -0400

    Finished implementing sequoia_nav_filter
    Implemented update equation; accounted for antenna offset; upsampled to 50 Hz from RTK data rate of 10 Hz.

--- sequoia_nav_filter/src/SequoiaNavFilter.cpp
+++ sequoia_nav_filter/src/SequoiaNavFilter.cpp
@@ -8,44 +8,40 @@ SequoiaNavFilter::SequoiaNavFilter(ros::NodeHandle n, ros::NodeHandle pn)
 {
   pn.param("parent_frame", parent_frame_, std::string("map"));
   pn.param("child_frame", child_frame_, std::string("base_footprint"));
+  pn.param("antenna_frame", antenna_frame_, std::string("gps_antenna"));
   pn.param("initial_heading", initial_heading_, 0.0);
 
-  sub_rtk = new message_filters::Subscriber<nav_msgs::Odometry>(n, "gps/rtkfixed", 1);
+  sub_rtk = new message_filters::Subscriber<nav_msgs::Odometry>(n, "gps/rtkfix", 1);
+  sub_vehicle_data = new message_filters::Subscriber<geometry_msgs::TwistStamped>(n, "/vehicle/twist", 1);
   sync_ekf_input_data = new message_filters::Synchronizer<EkfDataPolicy>(EkfDataPolicy(10), *sub_vehicle_data, *sub_rtk);
   sync_ekf_input_data->registerCallback(boost::bind(&SequoiaNavFilter::recvData, this, _1, _2));
   pub_heading = n.advertise<std_msgs::Float64>("ekf_heading", 1);
+  pub_odom = n.advertise<nav_msgs::Odometry>("ekf_odom", 1);
   srv_.setCallback(boost::bind(&SequoiaNavFilter::reconfig, this, _1, _2));
 
-  last_data_stamp_ = ros::Time(0);
+  last_stamp_ = ros::Time(0);
+  got_gps_ant_offset_ = false;
+  stationary_ = true;
+  tf_lookup_try_count_ = 0;
+
+  upsample_timer = n.createTimer(ros::Duration(0.02), &SequoiaNavFilter::timerCb, this);
 }
 
-void SequoiaNavFilter::recvData(const geometry_msgs::TwistStampedConstPtr& vehicle_data,
-                                const nav_msgs::OdometryConstPtr& rtk_data)
+void SequoiaNavFilter::timerCb(const ros::TimerEvent& event)
 {
-  // Initialize filter with first sample
-  if (last_data_stamp_ == ros::Time(0)) {
-    X(POS_X) = rtk_data->pose.pose.position.x;
-    X(POS_Y) = rtk_data->pose.pose.position.y;
-    X(HEADING) = initial_heading_;
-    X(SPEED) = vehicle_data->twist.linear.x;
-    X(YAW_RATE) = vehicle_data->twist.angular.z;
-
-    P.setIdentity();
-
-    last_data_stamp_ = rtk_data->header.stamp;
+  if (last_stamp_ == ros::Time(0) || stationary_) {
     return;
   }
 
-  // Lock filter updates if not moving
-  if (vehicle_data->twist.linear.x < 0.01) {
-    publishOutputs(rtk_data->header.stamp);
-    return;
-  }
+  double sample_time = (event.current_real - last_stamp_).toSec();
+  last_stamp_ = event.current_real;
 
-  // Compute sample time from delta since last measurement input
-  double sample_time = (rtk_data->header.stamp - last_data_stamp_).toSec();
-  last_data_stamp_ = rtk_data->header.stamp;
+  EkfPrediction(sample_time, X, P);
+  publishOutputs(event.current_real);
+}
 
+void SequoiaNavFilter::EkfPrediction(double sample_time, Matrix<double, 5, 1>& predicted_state, Matrix<double, 5, 5>& predicted_cov)
+{
   // Prediction step
   Matrix<double, 5, 5> A;
   A.row(0) << 1, 0, -sample_time * X(SPEED) * sin(X(HEADING)), sample_time * cos(X(HEADING)), 0;
@@ -54,25 +50,81 @@ void SequoiaNavFilter::recvData(const geometry_msgs::TwistStampedConstPtr& vehic
   A.row(3) << 0, 0, 0, 1, 0;
   A.row(4) << 0, 0, 0, 0, 1;
 
-  Matrix<double, 5, 1> predicted_state;
   predicted_state(0) = X(0) + sample_time * X(SPEED) * cos(X(HEADING));
   predicted_state(1) = X(1) + sample_time * X(SPEED) * sin(X(HEADING));
   predicted_state(2) = X(2) + sample_time * X(YAW_RATE);
   predicted_state(3) = X(3);
   predicted_state(4) = X(4);
 
-  Matrix<double, 5, 5> predicted_cov;
   predicted_cov = A * P * A.transpose() + Q;
+}
+
+void SequoiaNavFilter::recvData(const geometry_msgs::TwistStampedConstPtr& vehicle_data,
+                                const nav_msgs::OdometryConstPtr& rtk_data)
+{
+  // Look up TF transform to get GPS antenna offset
+  if (!got_gps_ant_offset_ && tf_lookup_try_count_ < 10) {
+    tf::StampedTransform transform;
+    try {
+      listener.lookupTransform("base_footprint", "gps_antenna", rtk_data->header.stamp, transform);
+      gps_ant_offset_ = transform.getOrigin();
+      got_gps_ant_offset_ = true;
+    } catch (tf::TransformException& ex) {
+      tf_lookup_try_count_++;
+    }
+  }
+
+  // Initialize filter with first sample
+  if (last_stamp_ == ros::Time(0)) {
+    X(POS_X) = rtk_data->pose.pose.position.x;
+    X(POS_Y) = rtk_data->pose.pose.position.y;
+    X(HEADING) = initial_heading_;
+    X(SPEED) = vehicle_data->twist.linear.x;
+    X(YAW_RATE) = vehicle_data->twist.angular.z;
+
+    P.setIdentity();
+
+    last_stamp_ = rtk_data->header.stamp;
+    return;
+  }
+
+  // Lock filter updates if not moving
+  if (vehicle_data->twist.linear.x < 0.01) {
+    stationary_ = true;
+    publishOutputs(rtk_data->header.stamp);
+    return;
+  } else {
+    stationary_ = false;
+  }
+
+  // Compute sample time from delta since last measurement input
+  double sample_time = (rtk_data->header.stamp - last_stamp_).toSec();
+  last_stamp_ = rtk_data->header.stamp;
+
+  Matrix<double, 5, 5> predicted_cov;
+  Matrix<double, 5, 1> predicted_state;
+  EkfPrediction(sample_time, predicted_state, predicted_cov);
 
   // Update Step
   Matrix<double, 6, 1> measurements;
   Matrix<double, 6, 1> expected_measurements;
   C.setZero();
-  // Update C matrix here
+  double c13 = -gps_ant_offset_.x() * sin(X(HEADING)) - gps_ant_offset_.y() * cos(X(HEADING));
+  double c23 = gps_ant_offset_.x() * cos(X(HEADING)) - gps_ant_offset_.y() * sin(X(HEADING));
+  C.row(0) << 1, 0, c13, 0, 0;
+  C.row(1) << 0, 1, c23, 0, 0;
+  C.row(2) << 0, 0, -X(SPEED) * sin(X(HEADING)), cos(X(HEADING)), 0;
+  C.row(3) << 0, 0, X(SPEED) * cos(X(HEADING)), sin(X(HEADING)), 0;
+  C.row(4) << 0, 0, 0, 1, 0;
+  C.row(5) << 0, 0, 0, 0, 1;
 
-  // Populate actual measurement vector here
+  measurements << rtk_data->pose.pose.position.x, rtk_data->pose.pose.position.y,
+                  rtk_data->twist.twist.linear.x, rtk_data->twist.twist.linear.y,
+                  vehicle_data->twist.linear.x, vehicle_data->twist.angular.z;
 
-  // Populate expected measurement vector here
+  double x_meas = X(POS_X) + gps_ant_offset_.x() * cos(X(HEADING)) - gps_ant_offset_.y() * sin(X(HEADING));
+  double y_meas = X(POS_Y) + gps_ant_offset_.x() * sin(X(HEADING)) + gps_ant_offset_.y() * cos(X(HEADING));
+  expected_measurements << x_meas, y_meas, X(SPEED) * cos(X(HEADING)), X(SPEED) * sin(X(HEADING)), X(SPEED), X(YAW_RATE);
 
   MatrixXd S = C * predicted_cov * C.transpose() + R;
   MatrixXd K = predicted_cov * C.transpose() * S.inverse();
@@ -81,12 +133,11 @@ void SequoiaNavFilter::recvData(const geometry_msgs::TwistStampedConstPtr& vehic
 
   X = predicted_state + K * error;
   P = (MatrixXd::Identity(5, 5) - K*C) * predicted_cov;
-
-  publishOutputs(rtk_data->header.stamp);
 }
 
 void SequoiaNavFilter::publishOutputs(const ros::Time& stamp)
 {
+  // TF frame
   tf::StampedTransform ekf_transform;
   ekf_transform.stamp_ = stamp;
   ekf_transform.frame_id_ = parent_frame_;
@@ -94,6 +145,18 @@ void SequoiaNavFilter::publishOutputs(const ros::Time& stamp)
   ekf_transform.setRotation(tf::createQuaternionFromYaw(X(HEADING)));
   ekf_transform.setOrigin(tf::Vector3(X(POS_X), X(POS_Y), 0));
   br.sendTransform(ekf_transform);
+
+  // Odometry
+  nav_msgs::Odometry odom_msg;
+  odom_msg.header.stamp = stamp;
+  odom_msg.header.frame_id = parent_frame_;
+  odom_msg.child_frame_id = child_frame_;
+  odom_msg.pose.pose.position.x = X(POS_X);
+  odom_msg.pose.pose.position.y = X(POS_Y);
+  odom_msg.pose.pose.orientation = tf::createQuaternionMsgFromYaw(X(HEADING));
+  pub_odom.publish(odom_msg);
+
+  // Heading`
   std_msgs::Float64 heading;
   heading.data = X(HEADING);
   pub_heading.publish(heading);
--- sequoia_nav_filter/src/SequoiaNavFilter.h
+++ sequoia_nav_filter/src/SequoiaNavFilter.h
@@ -9,6 +9,7 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <tf/tf.h>
+#include <nav_msgs/Odometry.h>
 
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
@@ -32,14 +33,18 @@ public:
 private:
   void reconfig(EkfConfig& config, uint32_t level);
   void recvData(const geometry_msgs::TwistStampedConstPtr& vehicle_data, const nav_msgs::OdometryConstPtr& rtk_data);
-
+  void timerCb(const ros::TimerEvent& event);
   void publishOutputs(const ros::Time& stamp);
+  void EkfPrediction(double sample_time, Eigen::Matrix<double, 5, 1>& predicted_state, Eigen::Matrix<double, 5, 5>& predicted_cov);
 
   message_filters::Subscriber<nav_msgs::Odometry>* sub_rtk;
   message_filters::Subscriber<geometry_msgs::TwistStamped>* sub_vehicle_data;
   message_filters::Synchronizer<EkfDataPolicy>* sync_ekf_input_data;
   ros::Publisher pub_heading;
+  ros::Publisher pub_odom;
+  ros::Timer upsample_timer;
   tf::TransformBroadcaster br;
+  tf::TransformListener listener;
   dynamic_reconfigure::Server<EkfConfig> srv_;
 
   Eigen::Matrix<double, 5, 5> Q;
@@ -48,10 +53,15 @@ private:
   Eigen::Matrix<double, 5, 5> P;
   Eigen::Matrix<double, 6, 5> C;
 
-  ros::Time last_data_stamp_;
+  ros::Time last_stamp_;
+  tf::Vector3 gps_ant_offset_;
   std::string child_frame_;
   std::string parent_frame_;
+  std::string antenna_frame_;
   double initial_heading_;
+  bool got_gps_ant_offset_;
+  bool stationary_;
+  int tf_lookup_try_count_;
 
 
 };
